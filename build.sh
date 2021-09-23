ccfb387e94f40d23ddb82a9dd79c17ff97feeb20
humpherys <mason.humpherys.p@gmail.com>
2021-09-23 16:06:16 -0600

    Added .gitignore and cleaned

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-exit 127
+echo "Linux cannot compile the iOS version of the app, so this script will only
+build the Android APK."
+
+flutter build apk
+if [ "$?" -ne "0" ]; then
+	  echo "Error with flutter build!"
+	  exit 1
+fi
+exit 0
