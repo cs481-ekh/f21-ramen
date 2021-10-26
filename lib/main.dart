e0ee1b4f74c3e4d0d925bedbd6dda23d5d547b98
chloejohnson <chloe@ChloesProgrammingPC.com>
2021-10-26 11:30:07 -0600

    Admin Schedule Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 // firebase plugins
 import 'package:firebase_core/firebase_core.dart';
@@ -89,6 +91,7 @@ class _AppState extends State<App> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
   final projectIdController = TextEditingController();
+  final adminProjectIdController = TextEditingController();
   bool _messagerInitialized = false;
 
   // Define an async function to initialize FlutterFire
@@ -224,6 +227,7 @@ class _AppState extends State<App> {
     usernameController.dispose();
     passwordController.dispose();
     projectIdController.dispose();
+    adminProjectIdController.dispose();
     super.dispose();
   }
 
@@ -235,7 +239,8 @@ class _AppState extends State<App> {
       home: LoginPage(
           usernameController: usernameController,
           passwordController: passwordController,
-          projectIdController: projectIdController),
+          projectIdController: projectIdController,
+          adminProjectIdController: adminProjectIdController),
     );
   }
 }
@@ -244,9 +249,10 @@ class LoginPage extends StatelessWidget {
   final TextEditingController usernameController;
   final TextEditingController passwordController;
   final TextEditingController projectIdController;
+  final TextEditingController adminProjectIdController;
 
   LoginPage(
-      {required this.usernameController, required this.passwordController, required this.projectIdController});
+      {required this.usernameController, required this.passwordController, required this.projectIdController, required this.adminProjectIdController});
 
   Future<void> onActionSelected(String value) async {
     switch (value) {
@@ -507,7 +513,7 @@ class LoginPage extends StatelessWidget {
                             context,
                             MaterialPageRoute(
                                 builder: (context) =>
-                                    isAdmin ? AdminPage() : UserPage()))
+                                    isAdmin ? AdminPage(adminProjectIdController: adminProjectIdController) : UserPage()))
                         : showDialog(
                             context: context,
                             builder: (BuildContext context) {
@@ -678,6 +684,12 @@ class UserPage extends StatelessWidget {
 }
 
 class AdminPage extends StatelessWidget {
+
+  final TextEditingController adminProjectIdController;
+
+  AdminPage(
+      {required this.adminProjectIdController});
+
   @override
   Widget build(BuildContext context) {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -695,35 +707,182 @@ class AdminPage extends StatelessWidget {
 
     // scaffold is a layout for the major Material Components
     return MaterialApp(
-      home: Scaffold(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
         appBar: AppBar(
           title: const Text('EMA - Administrator'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: "Home"),
+              Tab(icon: Icon(Icons.cases_outlined), text: "Projects"),
+            ]
+          )
         ),
         // body is majority of the screen
-        body: Center(
-          child: Column(
-            children: [
-              // OutlinedButton(
-              //   onPressed: addUser,
-              //   child: Text('Add User'),
-              // ),
-              // OutlinedButton(
-              //   onPressed: checkUser,
-              //   child: Text('Check User'),
-              // ),
-              OutlinedButton(
-                onPressed: signOut,
-                child: Text('Sign Out'),
-              ),
-            ],
-          ),
-        ),
-        // floatingActionButton: const FloatingActionButton(
-        //     tooltip: 'Send Reminder',
-        //     child: Icon(Icons.add),
-        //     onPressed: null,
-        // ),
+        body: TabBarView(
+    children: [
+      AdminHomePage(adminProjectIdController: adminProjectIdController),
+      AdminProjectPage(),
+      ]
       ),
+    )
+    )
+    );
+  }
+}
+
+class AdminHomePage extends StatefulWidget {
+  final TextEditingController adminProjectIdController;
+
+  const AdminHomePage ({ Key? key, required this.adminProjectIdController }): super(key: key);
+
+  @override
+  AdminHomePageState createState() => AdminHomePageState();
+}
+class AdminHomePageState extends State<AdminHomePage> {
+
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now().add(const Duration(days: 1));
+  DateTime? _selectedDay = DateTime.now();
+  DateTime? _time = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    auth.userChanges().listen((User? user) {
+      if (user == null) {
+        print("Signed OUT");
+      } else {
+        print('Signed IN');
+      }
+    });
+
+    Future<void> signOut() {
+      return auth.signOut();
+    }
+
+    // scaffold is a layout for the major Material Components
+    return MaterialApp(
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            // body is majority of the screen
+            body: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                    child: TextField(
+                      controller: widget.adminProjectIdController,
+                      obscureText: false,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Project ID',
+                      ),
+                    ),
+            ),
+                    TableCalendar(
+                        firstDay: DateTime.now(),
+                        lastDay: DateTime.now().add(const Duration(days: 365)),
+                        focusedDay: _focusedDay,
+                      selectedDayPredicate: (day) {
+                        return isSameDay(_selectedDay, day);
+                      },
+                      onDaySelected: (selectedDay, focusedDay) {
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _focusedDay = focusedDay; // update `_focusedDay` here as well
+                        });
+                        DatePicker.showTimePicker(context, showTitleActions: true, showSecondsColumn: false, onConfirm: (time) {
+                          setState(() {
+                            _time = time;
+                          });
+                        }, currentTime: DateTime.now());
+                      },
+                      calendarFormat: _calendarFormat,
+                      onFormatChanged: (format) {
+                        setState(() {
+                          _calendarFormat = format;
+                        });
+                      },
+                      onPageChanged: (focusedDay) {
+                          _focusedDay = focusedDay;
+                      },
+                      rowHeight: 40.0,
+                    ),
+                  Text('Send notification at ${_time?.hour}:${_time?.minute} on ${_selectedDay?.month}/${_selectedDay?.day}/${_selectedDay?.year}?',),
+                  TextButton(onPressed: () {
+                    print('Scheduled');
+                  }, child:
+                    const Text('Schedule'),),
+                  TextButton(onPressed: () {
+                    Navigator.pop(context);
+                  }, child:
+                  const Text('Logout'),),
+                  // OutlinedButton(
+                  //   onPressed: addUser,
+                  //   child: Text('Add User'),
+                  // ),
+                  // OutlinedButton(
+                  //   onPressed: checkUser,
+                  //   child: Text('Check User'),
+                  // ),
+                ],
+              ),
+            ),
+            // floatingActionButton: const FloatingActionButton(
+            //     tooltip: 'Send Reminder',
+            //     child: Icon(Icons.add),
+            //     onPressed: null,
+            // ),
+          ),
+        )
+    );
+  }
+}
+
+class AdminProjectPage extends StatelessWidget {
+
+  const AdminProjectPage({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // scaffold is a layout for the major Material Components
+    return MaterialApp(
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            // body is majority of the screen
+            body: Center(
+              child: Column(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 80.0, bottom: 20.0, left: 20.0, right: 20.0),
+                    child: Text('Projects'),
+                  ),
+                  // OutlinedButton(
+                  //   onPressed: addUser,
+                  //   child: Text('Add User'),
+                  // ),
+                  // OutlinedButton(
+                  //   onPressed: checkUser,
+                  //   child: Text('Check User'),
+                  // ),
+                ],
+              ),
+            ),
+            // floatingActionButton: const FloatingActionButton(
+            //     tooltip: 'Send Reminder',
+            //     child: Icon(Icons.add),
+            //     onPressed: null,
+            // ),
+          ),
+        )
     );
   }
 }
