b64cd0643a884fdce57d706dddf166475fe26724
Paisley Davis <paisley1000@gmail.com>
2021-11-01 16:24:42 -0600

    did login and user screens/functions

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,253 @@
+import 'package:ema/screens/user_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../actions/login_actions.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+  final TextEditingController projectIdController;
+  final TextEditingController adminProjectIdController;
+
+  LoginPage(
+      {Key? key, required this.usernameController,
+        required this.passwordController,
+        required this.projectIdController,
+        required this.adminProjectIdController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // FirebaseAuth auth = FirebaseAuth.instance;
+    // auth.userChanges().listen((User? user) {
+    //   if (user == null) {
+    //     // TODO: if user is not logged in, navigate to login/register page
+    //     print('Signed OUT');
+    //   } else {
+    //     print('Signed IN');
+    //   }
+    // });
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EMA'),
+        //This is just for testing and should be removed once a system is in
+        //place to subscribe devices based on users
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: onActionSelected,
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  value: 'subscribe',
+                  child: Text('Subscribe to topic'),
+                ),
+                const PopupMenuItem(
+                  value: 'unsubscribe',
+                  child: Text('Unsubscribe to topic'),
+                )
+              ];
+            },
+          ),
+        ],
+      ),
+      // body is majority of the screen
+      body: Center(
+        child: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 20.0, bottom: 20.0, left: 20.0, right: 20.0),
+                child: TextField(
+                  controller: usernameController,
+                  obscureText: false,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 3,
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  )),
+            ),
+            Flexible(
+              flex: 3,
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: projectIdController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Project ID',
+                    ),
+                  )),
+            ),
+            Flexible(
+                flex: 3,
+                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          padding: const EdgeInsets.only(
+                              top: 20.0, bottom: 20.0, left: 30.0, right: 30.0),
+                          primary: Colors.white,
+                          textStyle: const TextStyle(fontSize: 20),
+                          backgroundColor: Colors.blue),
+                      onPressed: () async {
+                        String userExists = await signinUser(usernameController, passwordController, projectIdController);
+                        // TODO: Set isAdmin if firebase username and password matches
+                        // admin username and password
+                        bool isAdmin = false;
+                        userExists == "" // navigate to appropriate user page
+                            ? Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => isAdmin
+                                    ? LoginPage(usernameController: usernameController, passwordController: passwordController, projectIdController: projectIdController, adminProjectIdController: adminProjectIdController)
+                        // AdminPage(
+                        //             adminProjectIdController:
+                        //             adminProjectIdController)
+                                    : UserPage()))
+                            : showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Error"),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: <Widget>[
+                                      Text(userExists),
+                                    ],
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            });
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.all(20.0),
+                          primary: Colors.white,
+                          textStyle: const TextStyle(fontSize: 20),
+                          backgroundColor: Colors.blue),
+                      onPressed: () async {
+                        bool isEmpty = usernameController.text == '' &&
+                            passwordController.text == '';
+                        RegExp exp = RegExp(r"\w+@.*\.(edu|com)");
+                        bool validEmail = exp.hasMatch(usernameController.text);
+                        String error = "";
+                        if (validEmail) {
+                          error = await addNewUser(usernameController, passwordController, projectIdController);
+                        }
+
+                        isEmpty
+                            ? showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Empty Username or Password"),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: const <Widget>[
+                                      Text("The required fields are empty."),
+                                    ],
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            })
+                            : validEmail
+                            ? error == ""
+                            ? Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage(usernameController: usernameController, passwordController: passwordController, projectIdController: projectIdController, adminProjectIdController: adminProjectIdController)))
+                            : showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text(
+                                    "Unable to register user."),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: <Widget>[
+                                      Text(error),
+                                    ],
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            })
+                            : showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("Email Invalid"),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: const <Widget>[
+                                      Text("Not a valid email."),
+                                    ],
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            });
+                      },
+                      child: const Text('Sign Up'),
+                    ),
+                  ),
+                ])),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/user_screen.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../actions/notification_actions.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  UserPageState createState() => UserPageState();
+}
+
+class UserPageState extends State<UserPage> {
+
+  late SharedPreferences _SharedPrefs;
+  List<String> MissedNotifs = [];
+  int notifAmount = 0;
+
+  @override
+  void initState() {
+    initializeSharedPrefs();
+    initializeMessageHandler();
+    super.initState();
+  }
+
+  void initializeSharedPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _SharedPrefs = prefs;
+    });
+    updateMissedNotifs();
+  }
+
+  void updateMissedNotifs() {
+    setState(() {
+      MissedNotifs = _SharedPrefs.getStringList("missedNotifs") ?? [];
+    });
+    setState(() {
+      notifAmount = MissedNotifs.length;
+    });
+    print("Updated notification list!");
+  }
+
+  //This function defines the widget built into the ListView
+  Widget listViewHelper(BuildContext context, int index) {
+
+    final notifJSON = MissedNotifs[index];
+    final nObject = jsonDecode(notifJSON);
+
+    //["id":idnumber,"received":time,"title":"Test","body":"This is a test notification","url":"test.com",]
+    final dateReceived = DateTime.parse(nObject['received']);
+    final notifInfo = '${nObject['title']} : ${nObject['body']}';
+    final url = nObject['url'];
+    final dateString = DateFormat('yyyy-MM-dd – h:mm a').format(dateReceived);
+
+    //In order to properly access the url object, this needs to be initialized here unfortunately
+    //Against everything I understand about Dart, it works so I'm not too worried
+    //If you can find another way to do it let me know
+    void openNotif() async {
+
+      var newNotifList = <String>[];
+      bool messageCheck = true;
+
+      for(final n in MissedNotifs) {
+        if(messageCheck) {
+          final temp = jsonDecode(n);
+          //Checks the message id, filters out the handled message to remove from storage
+          if (temp['id'] == nObject['id']) {
+            messageCheck = false;
+          } else {
+            newNotifList.add(n);
+          }
+        } else {
+          //Once we've found the matching message doing unnecessary json decoding is slow
+          newNotifList.add(n);
+        }
+      }
+
+      _SharedPrefs.setStringList("missedNotifs", newNotifList);
+
+      updateMissedNotifs();
+
+      if(url != null) {
+        if (await canLaunch(url)) {
+          await launch(url);
+        } else {
+          throw "Could not launch $url";
+        }
+      }
+    }
+
+    //This part returns the actual widget, along with a pointer to the tap function
+    return Card(
+        child: ListTile(
+          title:Text(notifInfo),
+          subtitle: Text(dateString),
+          onTap: openNotif,
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EMA'),
+      ),
+      // body is majority of the screen
+      body: Center(
+        child: Column(
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Notifications',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue))),
+            Flexible(
+                flex: 5,
+                child: Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: ListView.builder(
+                        padding: const EdgeInsets.all(5),
+                        itemCount: notifAmount,
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemBuilder: listViewHelper))),
+            Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.all(20.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                      backgroundColor: Colors.blue),
+                  onPressed: () {
+                    _SharedPrefs.setStringList("missedNotifs", []);
+                    setState(() {
+                      MissedNotifs = [];
+                      notifAmount = 0;
+                    });
+                  },
+                  child: const Text('Dismiss All'),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
